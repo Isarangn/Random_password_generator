ce5125f03e08d15229985169acd1e5307cd1d3b2
sarang <sarang.newaskar@gmail.com>
2020-08-12 19:04:53 +0530

    password.html page created

--- generator/views.py
+++ generator/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def password(request):
+    return render(request, 'password.html')
